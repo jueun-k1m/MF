2cba4df88be1444e168db1e2deb22d896c926131
jueun-k1m <jueunkim.ee@gmail.com>
2025-11-25 17:17:24 +0900

    Update arduino.py
    - 데이터 읽기만 일단 썼음
    - command 1, 2, 3, 4 넣어야함 !

--- omnitor/omnitor/src/device/arduino.py
+++ omnitor/omnitor/src/device/arduino.py
@@ -13,27 +13,13 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omnitor.settings")
 django.setup()
 
-from omnitor.models import SensorData , SensorCalibration
+from omnitor.models import RawData
 
 
 # 변수 설정 
 arduino_baudrate = 9600 # 시리얼 통신
 
-moving_average_window = 5 # 이동 평균 필터에서 윈도우 크기
-save_data_sec = 60 # 데이터 저장하는 초 단위
-read_data_sec = 1 # 데이터 읽는 초 단위
-
-# 이동 평균을 위한 데이터 버퍼 (Deque)
-data_buffer = {
-    'air_temperature': deque(maxlen=moving_average_window),
-    'air_humidity': deque(maxlen=moving_average_window),
-    'co2': deque(maxlen=moving_average_window),
-    'insolation': deque(maxlen=moving_average_window),
-    'weight_raw': deque(maxlen=moving_average_window),
-    'ph_raw': deque(maxlen=moving_average_window),
-    'ec_raw': deque(maxlen=moving_average_window),
-    'water_temperature': deque(maxlen=moving_average_window),
-}
+save_data_sec = 1 # 데이터 저장하는 초 단위
 
 
 def find_arduino_port(): 
@@ -48,7 +34,7 @@ def find_arduino_port():
     return None
 
 
-def read_data(ser):
+def save_data(ser):
 
     """ 아두이노에서 데이터 읽기 함수 """
 
@@ -58,76 +44,27 @@ def read_data(ser):
             parts = line.split(',')
 
             if len(parts) == 9:
-                data = {
-                    'timestamp': datetime.now(),
-                    'air_temperature': float(parts[0]),
-                    'air_humidity': float(parts[1]),
-                    'co2': float(parts[2]),
-                    'insolation': float(parts[3]),
-                    'weight_raw': float(parts[4]),
-                    'ph_raw': float(parts[5]),
-                    'ec_raw': float(parts[6]),
-                    'water_temperature': float(parts[7]),
-                    'tip_count': float(parts[8])
-                }
-                return data
+                try:
+                    RawData.objects.create(
+                        timestamp=datetime.now(),
+                        air_temperature=float(parts[0]),
+                        air_humidity=float(parts[1]),
+                        co2=float(parts[2]),
+                        insolation=float(parts[3]),
+                        weight_raw=float(parts[4]),
+                        ph_raw=float(parts[5]),
+                        ec_raw=float(parts[6]),
+                        water_temperature=float(parts[7]),
+                        tip_count=int(parts[8])
+                    )
+                except Exception as e:
+                    print(f"데이터베이스 저장 중 에러 발생: {e}")
+                
             
         except ValueError:
             print("데이터 변환 오류입니다.")
-    return None
+    return
 
-    
-def moving_avg_filter(data, data_buffer):
-
-    """ 이동 평균 필터 적용 및 데이터베이스 저장 함수 """
-    
-    avg_data = {}
-    
-    for key, value in data.items():
-        if key in data_buffer:
-            data_buffer[key].append(value) # 큐에 새로운 값 추가
-            avg_data[key] = round(statistics.mean(data_buffer[key]), 2)  # 이동 평균 계산
-        else:
-            avg_data[key] = value  # 버퍼가 없으면 원래 값 사용        
-    return avg_data # 평균 데이터 반환
-   
-
-def update_latest_raw_data(avg_data):
-
-    """ 최신 raw 데이터를 DB에 저장하는 함수 (보정용) """
-
-    try:
-        SensorCalibration.objects.update_or_create(
-            id=2,
-            defaults={
-                'ph_raw': avg_data['ph_raw'],
-                'ec_raw': avg_data['ec_raw'],
-                'weight_raw': avg_data['weight_raw'],
-                'water_temperature': avg_data['water_temperature'],
-            }
-        )
-    except Exception as e:
-        print(f"최신 raw 데이터 업데이트 중 에러 발생: {e}")
-
-
-def save_to_database(avg_data, tip_count):
-
-    """ 데이터베이스에 데이터 저장 함수 """
-
-    try:
-        SensorData.objects.create(
-            air_temperature=avg_data['air_temperature'],
-            air_humidity=avg_data['air_humidity'],
-            co2=avg_data['co2'],
-            insolation=avg_data['insolation'],
-            water_temp=avg_data['water_temperature'],
-            weight_raw=avg_data['weight'],
-            ph_raw=avg_data['ph'],
-            ec_raw=avg_data['ec'],
-            tip_count=tip_count
-        )
-    except Exception as e:
-        print(f"데이터베이스 저장 중 에러 발생: {e}")
 
 def main():
 
@@ -138,8 +75,7 @@ def main():
     
     ser = None
 
-    last_read_time = time.time()
-    last_save_time = time.time()
+    last_read_time = 0
 
     while True:
 
@@ -164,20 +100,9 @@ def main():
                 continue  # 포트를 찾을 때까지 대기
 
             
-            # 데이터 읽기
-            if current_time - last_read_time >= read_data_sec:
-                data = read_data(ser) # 아두이노에서 데이터 읽기
-
-                if data:
-                    avg_data = moving_avg_filter(data, data_buffer) # 이동 평균 필터 적용
-                    update_latest_raw_data(avg_data) # 최신 데이터 업데이트 (보정용)
-                    print(f"[{data['timestamp']}] 읽은 데이터: {avg_data}")
-                    
-                    if current_time - last_save_time >= save_data_sec:
-                        save_to_database(avg_data, data['tip_count']) # 데이터베이스 저장
-                        print(f"[{data['timestamp']}] 데이터 저장 완료: {avg_data}")
-                        last_save_time = current_time # 저장 시간 업데이트
-
+            # 데이터 읽기 및 저장
+            if current_time - last_read_time >= save_data_sec:
+                save_data()
                 last_read_time = current_time
 
             time.sleep(0.05)  # CPU 사용량 절감을 위한 짧은 대기
