323df7da57add61910821fa66406eaa9e8b28711
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-01 17:58:06 +0900

    Integrate soil sensor data into raw data saving

--- omnitor/omnitor/services/save_rawdata.py
+++ omnitor/omnitor/services/save_rawdata.py
@@ -16,28 +16,38 @@ django.setup()
 from omnitor.models import RawData # DB 모델
 from django.db import connection   # DB 연결 관리용
 from devices.arduino import SerialSingleton
+from devices.soil import SoilSensorSingleton
 
 def sensor_loop():
     arduino = SerialSingleton.instance()
 
     arduino.start()
 
+    soil = SoilSensorSingleton.instance()
+    soil.start()
+
+
     while True:
         try:
-            data = arduino.get_current_data()
+            arduino_data = arduino.get_current_data()
+            soil_data = soil.read()
 
-            if data:
+            if arduino_data and soil_data:
                 RawData.objects.create(
                     timestamp=datetime.now(),
-                    air_temperature=data.air_temperature,
-                    air_humidity=data.air_humidity,
-                    co2=data.co2,
-                    insolation=data.insolation,
-                    weight_raw=data.weight_raw,
-                    ph_voltage=data.ph_voltage,
-                    ec_voltage=data.ec_voltage,
-                    water_temperature=data.water_temperature,
-                    tip_count=data.tip_count
+                    air_temperature=arduino_data.air_temperature,
+                    air_humidity=arduino_data.air_humidity,
+                    co2=int(arduino_data.co2),
+                    insolation=arduino_data.insolation,
+                    weight_raw=int(arduino_data.weight_raw),
+                    ph_voltage=arduino_data.ph_voltage,
+                    ec_voltage=arduino_data.ec_voltage,
+                    water_temperature=arduino_data.water_temperature,
+                    tip_count=int(arduino_data.tip_count),
+                    soil_temperature=soil_data.soil_temperature,
+                    soil_humidity=soil_data.soil_humidity,
+                    soil_ec=soil_data.soil_ec,
+                    soil_ph=soil_data.soil_ph
                 )
 
                 connection.close()
@@ -52,6 +62,7 @@ def sensor_loop():
         except KeyboardInterrupt:
             break
     arduino.stop()
+    soil.stop()
 
 if __name__ == '__main__':
     sensor_loop()
