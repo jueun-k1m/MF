61f347bfa84fe9384f3c97760d974aa98d0f0b17
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-09 17:22:02 +0900

    Update and rename filter.py to filtering.py
    - maf_all() : RawData에 이동 평균 필터 적용 함수
    - avg(field_name) : 평균값 내는 함수

--- omnitor/omnitor/services/filtering.py
+++ omnitor/omnitor/services/filtering.py
@@ -1,11 +1,9 @@
-# help from gemini
-
 from omnitor.models import RawData
 from django.forms.models import model_to_dict
 
 WINDOW_SIZE = 5
 
-# 평균을 낼 필드 목록을 미리 정의 (timestamp 등 제외)
+# 평균을 낼 필드 목록을 미리 정의
 TARGET_FIELDS = [
     'air_temperature', 'air_humidity', 'co2', 'insolation', 
     'weight', 'ph', 'ec', 'water_temperature', 
@@ -13,21 +11,25 @@ TARGET_FIELDS = [
 ] 
 
 def maf_all():
+    
     """
     DB에서 최신 window_size개를 가져와 평균을 계산하여 반환
+    RawData -> FinalData 전처리 할 때 사용
+    :return: 필드별 평균값 딕셔너리
     """
-    # 1. 최신 데이터 N개 가져오기 (내림차순 정렬 후 슬라이싱)
+
+    # 최신 데이터 WINDOW_SIZE 개 가져오기 (내림차순 정렬 후 슬라이싱)
     latest_records = RawData.objects.order_by('-timestamp')[:WINDOW_SIZE]
 
     if not latest_records:
         return None
 
-    # 2. 데이터를 딕셔너리 리스트로 변환
+    # 데이터를 딕셔너리 리스트로 변환
     data_list = [model_to_dict(record) for record in latest_records]
 
     filtered_result = {}
 
-    # 3. 각 필드별로 평균 계산
+    # 각 필드별로 평균 계산
     for field in TARGET_FIELDS:
         # 해당 필드의 값들만 모음 (None 제외)
         values = [d[field] for d in data_list if d.get(field) is not None]
@@ -38,3 +40,23 @@ def maf_all():
             filtered_result[field] = 0 # 혹은 None
 
     return filtered_result
+
+
+def avg(field_name):
+
+    """
+    DB에서 특정 필드의 최신 값 WINDOW_SIZE개를 가져와 평균을 계산하여 반환
+    CalibrationData filtered 값 저장할 때 사용
+    :param field_name: 평균을 낼 필드 이름
+    :return: 해당 필드의 평균 값
+    """
+
+    val_5 = RawData.objects.values_list(field_name, flat=True).order_by('-timestamp')[:WINDOW_SIZE]
+
+    if not val_5:
+        return None
+    
+    avg_val = sum(val_5) / len(val_5)
+    
+    
+    return avg_val
