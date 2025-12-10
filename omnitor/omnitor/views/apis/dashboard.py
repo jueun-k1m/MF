fdf80380e159ef28957e2378b61daa98097f2cf3
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-10 09:57:50 +0900

    Refactor dashboard API for data retrieval and response

--- omnitor/omnitor/views/apis/dashboard.py
+++ omnitor/omnitor/views/apis/dashboard.py
@@ -1,44 +1,84 @@
-# views.py
 import json
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseNotAllowed
 from models import FinalData
 
+
 def dashboard_api(request):
+
     """
     [API] 대시보드 데이터 조회
     """
-    # 1. GET 요청인지 확인 (보안 및 규격 준수)
-    if request.method != 'GET':
-        return HttpResponseNotAllowed(['GET']) # 405 Method Not Allowed
 
-    try:
-        # 2. DB 조회
-        final_data = FinalData.objects.last()
+    # ======== GET: 날짜별 일지 & 사진 조회 ========
+    if request.method == 'GET':
 
-        # 3. 데이터가 없는 경우 404 에러 반환
-        if not final_data:
-            return HttpResponseNotFound("아직 수집된 센서 데이터가 없습니다.") # 404 Not Found
+        try:
+            # DB 조회
+            latest_data = FinalData.objects.order_by('-timestamp').first()
+            if not latest_data:
+                return HttpResponseNotFound("아직 수집된 센서 데이터가 없습니다.") # 404 Not Found
+            
+            try:
+                prev_data = latest_data.get_previous_by_timestamp()
+            except FinalData.DoesNotExist:
+                prev_data = None
 
-        # 4. 성공 시 데이터 구성
-        response_data = {
-            'timestamp': final_data.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
-            'air_temperature': final_data.air_temperature,
-            'air_humidity': final_data.air_humidity,
-            'co2': final_data.co2,
-            'insolation': final_data.insolation,
-            'weight': final_data.weight,
-            'water_temperature': final_data.water_temperature,
-            'ph': final_data.ph,
-            'ec': final_data.ec,
-            'soil_temperature': final_data.soil_temperature,
-            'soil_humidity': final_data.soil_humidity,
-            'soil_ph': final_data.soil_ph,
-            'soil_ec': final_data.soil_ec,
-        }
+            # 데이터가 없는 경우 에러 반환
+            if not latest_data:
+                return HttpResponseNotFound("아직 수집된 센서 데이터가 없습니다.") # 404 Not Found
 
-        return JsonResponse(response_data) # 200 OK (기본값)
 
-    except Exception as e:
-        # 서버 내부에서 예상치 못한 에러가 터졌을 때
-        print(f"Server Error: {e}")
-        return JsonResponse({'error': '서버 내부 오류가 발생했습니다.'}, status=500)
+            # VPD 계산
+            temp = latest_data.air_temperature,
+            hum = latest_data.air_humidity,
+            vpd = ((0.6107 * 10 ** (7.5 * temp / (237.3 + temp))) * (1 - (hum / 100)))
+
+            
+            # 누적 함수량 계산
+            instant_irrigation = 0
+            if prev_data:
+                instant_irrigation = latest_data.weight - prev_data.weight
+    
+            sum_insolation = 0
+
+
+
+            # 데이터
+            response_data = {
+                'timestamp': latest_data.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+
+                # 환경
+                'air_temperature': temp,
+                'air_humidity': hum,
+                'co2': latest_data.co2,
+                'insolation': latest_data.insolation,
+                'cumulative_insolation': sum_insolation,
+                'VPD' : vpd,
+                
+                # 함수량
+                'weight': latest_data.weight,
+                'irrigation' : latest_data.weight - prev_data.weight,
+                'irrigation_total' : sum_irrigation,
+                'drainage' : latest_data.tip_total,
+
+                # 배액
+                'water_temperature': latest_data.water_temperature,
+                'ph': latest_data.ph,
+                'ec': latest_data.ec,
+                
+                # 토양
+                'soil_temperature': latest_data.soil_temperature,
+                'soil_humidity': latest_data.soil_humidity,
+                'soil_ph': latest_data.soil_ph,
+                'soil_ec': latest_data.soil_ec
+
+            }
+            
+            prev_data = latest_data
+
+            return JsonResponse(response_data) # 200 OK (기본값)
+
+        except Exception as e:
+            # 서버 내부에서 예상치 못한 에러가 터졌을 때
+            print(f"Server Error: {e}")
+            return JsonResponse({'error': '서버 내부 오류가 발생했습니다.'}, status=500)
