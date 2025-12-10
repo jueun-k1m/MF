287862f77cc098514b9e90e5dfc3e8ec708db8ec
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-10 18:12:32 +0900

    Add imports for views and APIs in views.py

--- omnitor/omnitor/views.py
+++ omnitor/omnitor/views.py
@@ -1,1 +1,18 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.conf import settings as django_settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from models import SensorData, CalibrationSettings, FarmJournal
+import json
+import time
+import statistics
+import math
+import os
 
+
+from views.pages.dashboard import dashboard_api
+from views.apis.calibrate import calibrate_api
+from views.apis.camera_time import camera_time_api
+from views.apis.journal import journal_api
