e774ed820f3848ea6d0d04393fa7e63cc9eea05a
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-16 17:22:32 +0900

    Delete gomojang1215 directory

--- gomojang1215/omnitor/omnitor/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .calibrate import calibrate_api
-from .camera_time import camera_time_api
-from .dashboard import dashboard_api
-from .graph import graph_api
-from .journal import journal_api
-
-from .pages import dashboard_view, calibration_view, journal_view, graph_view
