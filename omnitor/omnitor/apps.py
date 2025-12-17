2703309bd178c551bf363c319091566ec8568d97
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-17 20:05:52 +0900

    Update apps.py

--- omnitor/omnitor/apps.py
+++ omnitor/omnitor/apps.py
@@ -1,30 +1,48 @@
 from django.apps import AppConfig
 import os
 import threading
+import time
+import schedule 
+
 from .devices.arduino import SerialSingleton
 from .devices.soil import SoilSensorSingleton
-from .services import schedule, save_data
-from .devices import camera
+
+def run_scheduler_loop():
+    print("[Debug] Run Scheduler Loop", flush=True)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 class OmnitorConfig(AppConfig):
     name = "omnitor"
 
     def ready(self):
-        # runserver autoreload 2번 실행 방지
         if os.environ.get("RUN_MAIN") != "true":
             return
 
-        # 아두이노 & 토양 센서 연결
         serial = SerialSingleton.instance()
         soil = SoilSensorSingleton.instance()
         serial.start()
         soil.start()
 
-        # rawdata & finaldata 저장하기 (schedule로)
-        schedule.add_time_interval_schedule("save_rawdata", 1, lambda: save_data.save_rawdata(serial, soil))
-        schedule.add_time_interval_schedule("save_finaldata", 57, save_data.save_finaldata)
+        from .services import schedule as my_schedule_service, save_data
         
+        my_schedule_service.add_time_interval_schedule(
+            "save_rawdata", 
+            1, 
+            lambda: save_data.save_rawdata(serial, soil)
+        )
+        
+        my_schedule_service.add_time_interval_schedule(
+            "save_finaldata", 
+            57, 
+            save_data.save_finaldata
+        )
+        
+        scheduler_thread = threading.Thread(target=run_scheduler_loop, daemon=True)
+        scheduler_thread.start()
+        print(">>> [System] Data Collector Scheduler Started!")
 
-        # 카메라 쓰레드로 실행      
+        from .devices import camera
         camera_thread = threading.Thread(target=camera.run_scheduler, daemon=True)
         camera_thread.start()
