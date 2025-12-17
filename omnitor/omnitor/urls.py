6707de7c6f8452db26c9c7ed057fd9175063428c
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-17 20:06:22 +0900

    Update urls.py

--- omnitor/omnitor/urls.py
+++ omnitor/omnitor/urls.py
@@ -1,33 +1,18 @@
-"""
-URL configuration for omnitor project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from . import views
+from .views import dashboard, calibrate, camera_time, journal, graph 
+from .views import pages
 
 urlpatterns = [
     # ========== pages =============
-    path('', views.pages.dashboard_view, name='dashboard_page'),
-    path('calibrate/', views.pages.calibrate_view, name='calibrate_page'),
-    path('journal/', views.pages.journal_view, name='journal_page'),
-    path('graph/', views.pages.graph_view, name='graph_page'),
+    path('', pages.dashboard_view, name='dashboard_page'),
+    path('calibrate/', pages.calibrate_view, name='calibrate_page'),
+    path('journal/', pages.journal_view, name='journal_page'),
+    path('graph/', pages.graph_view, name='graph_page'),
 
-    # =========== views ============
-    path('dashboard_api/', views.dashboard.dashboard_api, name='dashboard_api'),
-    path('calibrate_api/', views.calibrate.calibrate_api, name='calibrate_api'),
-    path('camera_time_api/', views.camera_time.camera_time_api, name='camera_api'),
-    path('journal_api/', views.journal.journal_api, name='journal_api'),
-    path('graph_api/', views.graph, name='graph_api'),
+    # =========== views (API) ============
+    path('dashboard_api/', dashboard.dashboard_api, name='dashboard_api'),
+    path('calibrate_api/', calibrate.calibrate_api, name='calibrate_api'),
+    path('camera_time_api/', camera_time.camera_time_api, name='camera_api'),
+    path('journal_api/', journal.journal_api, name='journal_api'),
+    path('graph_api/', graph.graph_api, name='graph_api'),
 ]
