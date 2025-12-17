3d0618e48304125c99f4e0e9f35a2f0eca264e6f
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-17 16:02:40 +0900

    Update schedule.py

--- omnitor/omnitor/services/schedule.py
+++ omnitor/omnitor/services/schedule.py
@@ -1,15 +1,15 @@
 import schedule
 
-def add_time_interval_shedule(id: str, second: float, job):
+def add_time_interval_schedule(id: str, second: float, job):
   schedule.every(second).seconds.do(job).tag(id)
 
-def add_timed_shedule(id: str, time_point: str, job):
+def add_timed_schedule(id: str, time_point: str, job):
   schedule.every().at(time_point).do(job).tag(id)
 
 def update_time_interval_schedule(id: str, second: float, job):
   schedule.clear(id)
-  add_time_interval_shedule(id, second, job)
+  add_time_interval_schedule(id, second, job)
 
 def update_timed_schedule(id: str, time_point: str, job):
   schedule.clear(id)
-  add_timed_shedule(id, time_point, job)
+  add_timed_schedule(id, time_point, job)
