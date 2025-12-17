31424dd000770f0bc7f456152a759569352e2c88
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-17 16:00:14 +0900

    Update pages.py

--- omnitor/omnitor/views/pages/pages.py
+++ omnitor/omnitor/views/pages/pages.py
@@ -4,19 +4,19 @@ from django.shortcuts import render
 # 1. 대시보드
 
 def dashboard_view(request):
-    return render(request, 'index.html')
+    return render(request, 'dashboard.html')
 
 # 2. 그래프
 
 def graph_view(request):
-    return render(request, 'index.html')
+    return render(request, 'graph.html')
 
 # 3. 농장 일지
 
 def journal_view(request):
-    return render(request, 'index.html')
+    return render(request, 'journal.html')
 
 # 4. 보정
 
 def calibration_view(request):
-    return render(request, 'index.html')
+    return render(request, 'calibrate.html')
