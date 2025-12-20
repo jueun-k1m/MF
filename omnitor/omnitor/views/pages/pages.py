ea992f9e1b61bfc34de4c81071373481ec313e51
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-20 21:59:07 +0900

    최신

---
+++ omnitor/omnitor/views/pages/pages.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# 1. 메인 대시보드 화면
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+# 2. 보정(Calibration) 설정 화면
+def calibrate_view(request):
+    return render(request, 'calibrate.html')
+
+# 3. 농장 일지 화면
+def journal_view(request):
+    return render(request, 'journal.html')
+
+# 4. 그래프 분석 화면
+def graph_view(request):
+    return render(request, 'graph.html')
