bafd80057016e5af421085d30a546f96e69d6a59
jueun-k1m <jueunkim.ee@gmail.com>
2025-12-20 21:53:33 +0900

    최신

--- omnitor/omnitor/services/filtering.py
+++ omnitor/omnitor/services/filtering.py
@@ -1,6 +1,4 @@
-# help from gemini
-
-from django.omnitor.omnitor.models.models import RawData
+from omnitor.models import RawData
 from django.forms.models import model_to_dict
 
 WINDOW_SIZE = 5
@@ -38,3 +36,23 @@ def maf_all():
             filtered_result[field] = 0 # 혹은 None
 
     return filtered_result
+
+def avg(field_name):
+    """
+    DB에서 특정 필드의 최신 값 WINDOW_SIZE개를 가져와 평균을 계산하여 반환
+    CalibrationData filtered 값 저장할 때 사용
+    :param field_name: 평균을 낼 필드 이름
+    :return: 해당 필드의 평균 값
+    """
+
+    # 1. 최신 데이터 5개 가져오기
+    val_5 = RawData.objects.values_list(field_name, flat=True).order_by('-timestamp')[:WINDOW_SIZE]
+
+    # 2. 데이터가 없으면 None 반환
+    if not val_5:
+        return 0
+    
+    # 3. 평균 계산
+    average_val = sum(val_5) / len(val_5) # (주의: 함수명과 변수명이 같음)
+    
+    return average_val
