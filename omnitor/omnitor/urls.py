f876e3e2f1237b58533a08dc2952e2316df51380
jueun-k1m <jueunkim.ee@gmail.com>
2026-01-06 13:39:16 +0900

    Update urls.py

--- omnitor/omnitor/urls.py
+++ omnitor/omnitor/urls.py
@@ -1,5 +1,11 @@
+"""
+URL configuration for omnitor project.
+"""
 from django.urls import path
-from .views import dashboard, calibrate, camera_time, journal, graph
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import api_dashboard, api_calibrate, api_journal, api_graph
 from .views import pages
 
 urlpatterns = [
@@ -9,10 +15,12 @@ urlpatterns = [
     path('journal/', pages.journal_view, name='journal_page'),
     path('graph/', pages.graph_view, name='graph_page'),
 
-    # =========== views (API) ============
-    path('dashboard_api/', dashboard.dashboard_api, name='dashboard_api'),
-    path('calibrate_api/', calibrate.calibrate_api, name='calibrate_api'),
-    path('camera_time_api/', camera_time.camera_time_api, name='camera_api'),
-    path('journal_api/', journal.journal_api, name='journal_api'),
-    path('graph_api/', graph.graph_api, name='graph_api'),
+    # =========== views ============
+    path('dashboard_api/', api_dashboard.dashboard_api, name='dashboard_api'),
+    path('calibrate_api/', api_calibrate.calibrate_api, name='calibrate_api'),
+    path('journal_api/', api_journal.journal_api, name='journal_api'),
+    path('graph_api/', api_graph.graph_api, name='graph_api'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
